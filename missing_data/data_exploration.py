36a97bc7a17fd6bda2f6d4721ff707ed4ac818f5
Joseph <j.moorhouse1898@gmail.com>
2019-10-14 20:47:33 +0100

    Added summary method

--- missing_data/data_exploration.py
+++ missing_data/data_exploration.py
@@ -3,3 +3,20 @@ import pandas as pd
 class MissingDataAnalysis(pd.DataFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
+    def summary(self, 
+                features = None, 
+                precision = 2, 
+                return_missing_only = True, 
+                col_name = "%_missing_data"):
+        
+        if isinstance(features, (list, tuple)):
+            i, j = features
+            
+        missing = self.iloc[:, i:j].isnull().mean().to_frame() * 100
+        missing.columns = [col_name]
+        
+        if return_missing_only:
+            return missing.loc[missing[col_name] > 0]
+        
+        return missing.round(precision)
