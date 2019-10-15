225d08e399ae09a299913b4eac9646294299f75e
Joseph <j.moorhouse1898@gmail.com>
2019-10-15 19:57:49 +0100

    Updated summary method

--- missing_data/data_exploration.py
+++ missing_data/data_exploration.py
@@ -1,29 +1,30 @@
 import pandas as pd
 
 class MissingDataAnalysis():
-    def __init__(self, data):
+    def __init__(self, data = None):
         self.data = data
         
-    def summary(self, 
+    def quality_summary(self, 
                 start = None,
                 stop = None,
                 step = None,
                 precision = 2, 
                 return_missing_only = True):
         
-        data = self.data
-        features = data.iloc[:, start : stop : step]
+        features = self.data.iloc[:, start : stop : step]
         
-        stat_columns = ["missing_count", "%_missing", "feature_type"]
-        data = np.array([
-            features.isnull().sum(),
-            (features.isnull().mean() * 100).round(2),
-            features.dtypes
-        ])
+        # summary data definitions
+        data = {
+            "count" : features.isnull().sum().astype(int),
+            "cardinality" : features.nunique(),
+            "%_missing" : (features.isnull().mean() * 100).round(2),
+            "feature_type" : features.dtypes
+        }
         
-        missing = pd.DataFrame(data.T, columns=stat_columns)
+        missing = pd.DataFrame.from_dict(data)
                 
+        # determine if only missing values are returned in the analysis
         if return_missing_only:
-            return missing.loc[missing["missing_count"] > 0]
+            return missing.loc[missing["count"] > 0]
         else:
             return missing
