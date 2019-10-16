8be2c5b9da99fd41885c32050b69197457e134e9
Joseph <j.moorhouse1898@gmail.com>
2019-10-16 15:19:50 +0100

    updated with missing_features property and updated formatting

--- missing_data/data_exploration.py
+++ missing_data/data_exploration.py
@@ -1,15 +1,40 @@
 import pandas as pd
 
+
 class MissingDataAnalysis():
     
-    # -----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
+    # Constructors
+    
     def __init__(self, data = None):
         self.data = data
+        self.missing = self._get_null(data)
         
+        
+    # ----------------------------------------------------------------------
+    # Non-public methods
+    
+    def _get_null(self, data):
+        df = data.copy()
+        
+        return df.loc[:, df.isnull().any()]
+    
+    
+    # ----------------------------------------------------------------------
+    # Properities
+    
     @property
-    def type_symmary(self):
+    def missing_features(self):
+        f = (self.missing.shape[1] / self.data.shape[1]) * 100
+        print("{}% of the features have missing data".format(int(f)))
+        
+        
+    # ----------------------------------------------------------------------
+    # Public methods 
+    
+    def type_summary(self, start=None, stop=None):
         # pd.Series of the distribution of dtypes
-        type_summary = self.data.dtypes.value_counts()
+        type_summary = self.missing.dtypes.value_counts()
         
         # define subplots
         fig, ax = plt.subplots(
@@ -17,17 +42,18 @@ class MissingDataAnalysis():
             subplot_kw=dict(aspect="equal")
         )
 
-        # generate wedge pie plot and define label attributes
+        # generate wedge pie plot 
         wedges, texts = ax.pie(
-            counts.values, 
-            wedgeprops=dict(width=0.4), 
+            type_summary.values, 
+            wedgeprops=dict(width=0.4),
             startangle=-40
         )
+        
+        # define label attributes
         bbox_props = dict(boxstyle="square,pad=0.5", fc="w", ec="k", lw=0.9)
         kw = dict(arrowprops=dict(arrowstyle="-"),
                   bbox=bbox_props, zorder=0, va="center", fontsize=12)
 
-        # annotate the wedges
         for i, p in enumerate(wedges):
             # set label arrow line angles
             ang = (p.theta2 - p.theta1)/2. + p.theta1
@@ -40,23 +66,28 @@ class MissingDataAnalysis():
             kw["arrowprops"].update({"connectionstyle": connectionstyle})
             
             # display the annotated labels
-            ax.annotate([str(i) for i in counts.index.tolist()][i], 
-                        xy=(x, y), 
-                        xytext=(1.35*np.sign(x), 1.4*y),
-                        horizontalalignment=horizontalalignment, 
-                        **kw)
+            ax.annotate(
+                [str(i) for i in type_summary.index.tolist()][i], 
+                xy=(x, y), 
+                xytext=(1.35*np.sign(x), 1.4*y),
+                horizontalalignment=horizontalalignment, 
+                **kw
+            )
 
-        ax.set_title("Data type summary", fontdict = {"fontsize" : 15})
+        ax.set_title("Missing data type summary", fontdict = {"fontsize":15})
         plt.show()
+        
         
     def quality_summary(self, 
                 start = None,
                 stop = None,
                 step = None,
-                precision = 2, 
-                return_missing_only = True):
+                precision = 2):
         
-        features = self.data.iloc[:, start : stop : step]
+        if start:
+            features = self.missing.iloc[:, start : stop : step]
+        else:
+            features = self.missing
         
         # summary data definitions
         data = {
@@ -66,10 +97,5 @@ class MissingDataAnalysis():
             "feature_type" : features.dtypes
         }
         
-        missing = pd.DataFrame.from_dict(data)
-                
-        # determine if only missing values are returned in the analysis
-        if return_missing_only:
-            return missing.loc[missing["count"] > 0]
-        else:
-            return missing
+        return pd.DataFrame.from_dict(data)
+        
