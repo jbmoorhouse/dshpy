645ab3dab8682dd2beb334227503f24f3e1e9500
Joseph <j.moorhouse1898@gmail.com>
2019-10-28 19:54:13 +0000

    Updated univariate plot

--- eda/exploratory_data_analysis.py
+++ eda/exploratory_data_analysis.py
@@ -1,5 +1,6 @@
-import seaborn as sns
 import pandas as pd
+import numpy as np
+
 from bokeh.io import show, output_notebook
 from bokeh.layouts import row, layout
 from bokeh.models import ColumnDataSource, CustomJS, DataRange1d, BoxSelectTool, LassoSelectTool
@@ -8,74 +9,51 @@ from bokeh.models.widgets import Select
 
 class EDA():
     
-    sns.set()
-    
+    output_notebook()
+     
     # ----------------------------------------------------------------------
     # Constructors
     
     def __init__(self, data):
         self.data = data
-        
-    def _grid_shape(self, n_features, n_cols):
-        """
-        Defines the plot grid shape to which feature plots are assigned.
-
-        Parameters
-        ----------
-        n_features : int
-            Number of features intended to be plotted
-        n_cols : int
-            Desired number of columns in the grid plot.
-
-        Returns
-        -------
-        rows : int 
-            Number of rows in the grid plot.
-        cols : int
-            Number of cols in the grid plot.
-        """
-
-        if n_features % n_cols == 0:
-            rows, cols = n_features / n_cols, n_cols
-        else:
-            rows, cols = (n_features // n_cols) + 1, n_cols
-
-        return int(rows), int(cols)
     
-        
-    def univariate(self, features, n_cols=2, style = 'dark'):
-        
-        #Add logic to prevent more than 25 plots in one go
-        
-        if isinstance(features, str):
-            features = [features]
-            
-        df = self.data[features]
-        rows, cols = self._grid_shape(df.shape[1], n_cols)
-        
-        df.hist(bins = 25, figsize=(50, 15 * rows), layout=(rows, cols))
-        
-    
-    def zero_inflated(self):
-        pass
-    
-    def bivariate_plot(self):
+    def _make_bivariate_dataset(self):
         df = self.data.copy()
         
         df['x'] = df.iloc[:, 0]
         df['y'] = df.iloc[:, 1]
         
+        return df
+    
+    def _make_univariate_dataset(self, bins, density):
+        df = self.data.copy()
+        
+        for col in df:
+            hist, edges = np.histogram(df[col], bins=bins, density=density)
+
+            yield pd.DataFrame({
+                "top_" + str(col) : hist,
+                "left_" + str(col) : edges[:-1],
+                "right_" + str(col) : edges[1:]
+            })
+        
+    # ----------------------------------------------------------------------
+    # Plotting
+    
+    def bivariate_plot(self):
+        df = self._make_bivariate_dataset()
+        
         p = figure(
             x_range = DataRange1d(range_padding=.1), 
-            y_range = DataRange1d(range_padding=.1),
+            y_range = DataRange1d(range_padding=0.1),
             background_fill_color = "#fafafa"
         )
-        
+
         source = ColumnDataSource(df)
         p.scatter('x', 'y', source=source)
         
-        # Custom javascript method for updating the plot axes.
         def _custom_js(source, col):
+
             return CustomJS(
                 args={'source' : source, 'col' : col}, code="""
                 var data = source.data;
@@ -84,15 +62,12 @@ class EDA():
                 """
             )
         
-        # Define Select box structure
         cols = df.columns.tolist()
-        
         x_select = Select(
-            title = "Select x:", 
+            title = "Select x:",
             value = cols[0], 
             options = cols[:-2]
         )
-        
         y_select = Select(
             title = "Select y:", 
             value = cols[1], 
@@ -102,8 +77,51 @@ class EDA():
         # Add the callback to the select widget. 
         y_select.callback = _custom_js(source = source, col='y')
         x_select.callback = _custom_js(source = source, col='x')
+        
+        l = layout([ [x_select, y_select], [p]])
+        show(l)
 
-        show(layout([[x_select, y_select], [p]]))
-    
-    def bokeh_univariate(self):
-        pass
+        
+    def univariate_plot(self, bins = 25, density = True):
+        concat = self._make_univariate_dataset(bins, density)
+        
+        df = pd.concat(*[ list(concat) ], axis=1)
+        df[['top', 'left', 'right']] = df.iloc[:, 0:3] 
+        
+        source = ColumnDataSource(new_df)
+        p = figure(tools='', background_fill_color="#fafafa")
+        
+        p.quad(
+            top = 'top',
+            bottom=0, 
+            left='left', 
+            right='right', 
+            source = source,
+            fill_color="navy", 
+            line_color="white", 
+            alpha=0.5
+        )
+        
+        def _custom_js(source):
+            return CustomJS(
+                args={'source' : source}, code="""
+                var data = source.data;
+                var prefix = cb_obj.value;
+
+                data['top'] = data['top_'.concat(prefix)];
+                data['left'] = data['left_'.concat(prefix)];
+                data['right'] = data['right_'.concat(prefix)];
+
+                source.change.emit();
+                """
+            )
+        
+        cols = self.data.columns.tolist()
+        x_select = Select(
+            title = "Select x:", 
+            value = cols[0], 
+            options = cols
+        )
+        x_select.callback = _custom_js(source = source)
+
+        show(layout([p, x_select]))
